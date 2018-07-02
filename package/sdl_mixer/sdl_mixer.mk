8ba0e7ad0659a273c6b873f86564bb7628cd6697
gameblabla <gameblabla@openmailbox.org>
2018-07-02 14:09:04 -0400

    libSDL_mixer fixxes : rebasing around version 1.2.13 (unreleased, source only), hacks for the RS-97 (fixes Ganbare & KETM with OGG music), more stable, fixes mod & mp3 support not being built

--- package/sdl_mixer/sdl_mixer.mk
+++ package/sdl_mixer/sdl_mixer.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-SDL_MIXER_VERSION = 1.2.12
-SDL_MIXER_SOURCE = SDL_mixer-$(SDL_MIXER_VERSION).tar.gz
-SDL_MIXER_SITE = http://www.libsdl.org/projects/SDL_mixer/release
+SDL_MIXER_VERSION = 1.2.13
+SDL_MIXER_SOURCE = SDL-1.2.tar.gz
+SDL_MIXER_SITE =  https://github.com/SDL-mirror/SDL_mixer/archive
 SDL_MIXER_LICENSE = zlib
 SDL_MIXER_LICENSE_FILES = COPYING
 
@@ -19,11 +19,18 @@ SDL_MIXER_CONF_OPTS = \
 	--disable-music-mod \
 	--disable-music-flac # configure script fails when cross compiling
 
-ifeq ($(BR2_PACKAGE_SDL_MIXER_LIBMAD),y)
+ifeq ($(BR2_PACKAGE_LIBMAD),y)
 SDL_MIXER_CONF_OPTS += --enable-music-mp3-mad-gpl
 SDL_MIXER_DEPENDENCIES += libmad
 else
 SDL_MIXER_CONF_OPTS += --disable-music-mp3-mad-gpl
+endif
+
+ifeq ($(BR2_PACKAGE_LIBMODPLUG),y)
+SDL_MIXER_CONF_OPTS += --enable-music-mod-modplug
+SDL_MIXER_DEPENDENCIES += libmodplug
+else
+SDL_MIXER_CONF_OPTS += --disable-music-mod-modplug
 endif
 
 # prefer tremor over libvorbis
